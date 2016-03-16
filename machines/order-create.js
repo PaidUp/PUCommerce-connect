5d5feed3de49653b85a6219db3fef60280314ff7
Ricardo Lara <riclara@gmail.com>
2016-03-16 14:54:29 -0500

    refactor name machines

--- machines/order-create.js
+++ machines/order-create.js
@@ -1,5 +1,3 @@
-var paymentPlan = require('../core/common/objects/paymentPlan')
-
 module.exports = {
 
   friendlyName: 'Order Create',
@@ -30,7 +28,47 @@ module.exports = {
       required : true
     },
     paymentsPlan : {
-      example : [paymentPlan],
+      example : [{
+        destinationId: 'destinationId',
+        dateCharge: '2016-05-05',
+        price: 100,
+        typeAccount: 'typeAccount',
+        account: 'account',
+        discount: 0, // optional default 0
+        discountCode: 'discountCode', // optional
+        wasProcessed: false,// optional default false
+        status: 'pending',// optional default pending
+        processingFees: {
+          cardFee: 12,
+          cardFeeActual: 21,
+          cardFeeFlat: 12,
+          cardFeeFlatActual: 21,
+          achFee: 12,
+          achFeeActual: 21,
+          achFeeFlat: 12,
+          achFeeFlatActual: 21
+        },
+        collectionsFee: {
+          fee: 12,
+          feeFlat: 21
+        },
+        paysFees: {
+          processing: true,
+          collections: true
+        },
+        productInfo: {
+          productId: 'productId',
+          productName: 'productName'
+        },
+        userInfo: {
+          userId: 'UserId',
+          userName: 'userName'
+        },
+        beneficiaryInfo: {
+          beneficiaryId: 'beneficiaryId',
+          beneficiaryName: 'beneficiaryName'
+        }
+      }],
       description : 'paymentsPlan to own order.',
       required : true
     }
