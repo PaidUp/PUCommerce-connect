caadc988cd344813bf0ea30107e165d2bd9ed645
Jesse Cogollo <cogollo87@gmail.com>
2016-04-01 11:39:06 -0500

    remove console log

--- machines/coupon-create.js
+++ machines/coupon-create.js
@@ -99,7 +99,6 @@ module.exports = {
     }
 
     Connector.request(config, {}, body, function (err, resp) {
-      console.log('err', err)
       if (err && err.status === 500) {
         return exits.error({
           status: err.status,
