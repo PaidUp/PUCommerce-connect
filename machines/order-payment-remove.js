d002d162ad2199b1fd65a5b6d8ba15eb1c34d83c
Ricardo Lara <riclara@gmail.com>
2017-01-06 11:21:00 -0500

    Add payment remove

---
+++ machines/order-payment-remove.js
@@ -0,0 +1,84 @@
+module.exports = {
+  friendlyName: 'Order Payment Remove',
+  description: 'remove payment plan by _id',
+  cacheable: false,
+  sync: false,
+
+  inputs: {
+    baseUrl : {
+      example : 'http://localhost:9002',
+      description : 'Url microservice.',
+      required : true
+    },
+    token : {
+      example : 'TDCommerceToken-CHANGE-ME!',
+      description : 'secret word for authenticate microservice.',
+      required : true
+    },
+    orderId : {
+      example : "100006",
+      description : 'ORDER ID',
+      required : true
+    },
+    paymentPlanId : {
+      example : "XXXXXXXX",
+      description : 'payment plan _id',
+      required : true
+    },
+    userSysId : {
+      example : "XXXXXXXXXXX",
+      description : 'user id',
+      required : true
+    }
+  },
+
+  exits: {
+
+    success: {
+      friendlyName: 'order',
+      description: 'order object',
+      example: {
+          status: 200,
+          body: {}
+        }
+    },
+
+    error: {
+      description: 'Some error',
+      example: {
+        status: 500,
+        message: '[{"maybe some JSON": "like this"}]  (but could be any string)'
+      }
+    }
+
+  },
+
+  fn: function(inputs, exits) {
+    var Connector  = require('../core/common/connector');
+
+    var config = {
+      url: '/api/v3/commerce/order/payments/remove',
+      baseUrl: inputs.baseUrl,
+      method: 'post',
+      token : inputs.token
+    };
+
+    Connector.request(config, {}, {
+      userSysId: inputs.userSysId,
+      orderId: inputs.orderId,
+      paymentPlanId: inputs.paymentPlanId
+    }, function(err, resp){
+      if(err){
+        return exits.error({
+          status: err.status,
+          message: err.body
+        });
+      }else{
+        return exits.success({
+          status: resp.status,
+          body: resp.body
+        });
+      }
+    });
+  }
+};
